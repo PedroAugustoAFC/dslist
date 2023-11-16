87763fc7725b64a290b9586700fb022d383a42c1
PedroAugustoAFC“git config --global password Pedro_06072003“ <PedroAugustoAFC0@hotmail.com>
2023-11-16 15:30:56 -0300

    Game seed

--- src/main/java/com/springproject/dslist/entities/Game.java
+++ src/main/java/com/springproject/dslist/entities/Game.java
@@ -19,22 +19,28 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
+    public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -72,12 +78,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -126,6 +140,7 @@ public class Game {
         return true;
     }
 
+    
     
 
 }
