00baf7b8630a54f6377b8157b5b0b4eb1459cfb4
PedroAugustoAFC“git config --global password Pedro_06072003“ <PedroAugustoAFC0@hotmail.com>
2023-11-16 19:49:17 -0300

    Domain Model

---
+++ src/main/java/com/springproject/dslist/entities/BelongingPK.java
@@ -0,0 +1,72 @@
+package com.springproject.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    public BelongingPK(){
+    }
+
+    public BelongingPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((game == null) ? 0 : game.hashCode());
+        result = prime * result + ((gameList == null) ? 0 : gameList.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BelongingPK other = (BelongingPK) obj;
+        if (game == null) {
+            if (other.game != null)
+                return false;
+        } else if (!game.equals(other.game))
+            return false;
+        if (gameList == null) {
+            if (other.gameList != null)
+                return false;
+        } else if (!gameList.equals(other.gameList))
+            return false;
+        return true;
+    }
+    
+}
